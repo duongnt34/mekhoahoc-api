7cb979d8d8e48b89452ea11261b4d7ae7c8444d8
duongnt34 <duongntdev@outlook.com.vn>
2023-08-25 14:46:56 +0700

    wip

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -20,26 +21,39 @@ class AuthController extends Controller
     {
         $credentials = $request->only('email', 'password');
         if(!$token = JWTAuth::attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+            return response()->json(['error' => 'Tài khoản hoặc mật khẩu không đúng'], Response::HTTP_UNAUTHORIZED);
         }
 
         $cookie = cookie('jwt', $token);
 
+        $user = auth()->user();
+
         return response()->json([
-            'success' => true
+            'success' => true,
+            'message' => 'Đăng nhập thành công',
+            'user' => $user,
         ])->withCookie($cookie);
 
     }
 
     public function user() {
-        return auth()->user();
+        if(!auth()->check()){
+            return response()->json(['error' => 'Người dùng chưa đăng nhập'], Response::HTTP_UNAUTHORIZED);
+        }
+        try {
+            $user = auth()->userOrFail();
+        } catch (\Tymon\JWTAuth\Exceptions\UserNotDefinedException $e) {
+            return response()->json(['error' => 'Không tìm thấy thông tin người dùng'], Response::HTTP_FORBIDDEN);
+        }
+        return $user;
     }
 
     public function logout(Request $request) {
         $cookie = Cookie::forget('jwt');
         auth()->logout(true);
         return response()->json([
-            'success' => true
+            'success' => true,
+            'message' => 'Đã đăng xuất'
         ], Response::HTTP_OK)->withCookie($cookie);
     }
 }
