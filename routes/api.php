6e4b0548d2ea69a6a373edefe7771c756ec48ac2
duongnt34 <duongntdev@outlook.com.vn>
2023-08-25 14:46:56 +0700

    wip

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,6 +23,5 @@ Route::group(['middleware'=> 'api'], function(){
         Route::post('logout', [AuthController::class, 'logout']);
         Route::post('refresh', [AuthController::class, 'refresh']);
     });
-
     Route::get('user', [AuthController::class, 'user']);
 });
