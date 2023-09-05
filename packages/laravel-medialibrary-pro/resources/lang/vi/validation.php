b66ca6a3ff1cb29b90e5553496bb2ff49675a004
duongnt34 <duongntdev@outlook.com.vn>
2023-09-05 16:21:15 +0700

    add media-library

---
+++ packages/laravel-medialibrary-pro/resources/lang/vi/validation.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'total_upload_size_too_high' => 'Kích thước cho phép tối đa là :max',
+    'total_upload_size_too_low' => 'Kích thước cho phép tối thiểu là :min',
+
+    'file_too_big' => 'Tệp này quá lớn. Kích thước tối đa cho phép :max',
+    'file_too_small' => 'Tệp này quá nhỏ. Kích thước ít nhất phải là :min',
+
+    'incorrect_dimensions' => [
+        'width' => 'Chiều rộng hình ảnh phải là :width pixels',
+        'height' => 'Chiều cao hình ảnh phải là :height pixels',
+        'both' => 'Kích thước hình ảnh phải là :width x :height pixels',
+    ],
+
+    'width_not_between' => 'Chiều rộng hình ảnh phải nằm trong khoảng :min và :max pixels',
+    'height_not_between' => 'Chiều cao hình ảnh phải nằm trong khoảng :min và :max pixels',
+
+    'max_items' => 'Bạn chỉ có thể tải lên :max mục | Bạn chỉ có thể tải lên :max mục',
+    'min_items' => 'Bạn phải tải lên ít nhất :min mục | Bạn phải tải lên ít nhất :min mục',
+
+    'extension' => 'Tệp phải thuộc loại :extensions',
+    'mime' => 'Tệp phải có thuộc loại :mimes',
+    'type' => 'Bạn phải tải lên một loại tệp :values',
+];
